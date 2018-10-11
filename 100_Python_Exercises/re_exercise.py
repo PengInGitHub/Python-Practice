5f4ecd8966fe791bf02138d66b70d0c1a14b7b44
PengInGitHub <felixdeutschland@hotmail.com>
2018-10-11 07:57:10 +0200

    add re-do exercises

--- 100_Python_Exercises/re_exercise.py
+++ 100_Python_Exercises/re_exercise.py
@@ -112,6 +112,260 @@ def cal_day(year, month, day):
         
 cal_day(1998, 12, 31)
 
+###############################
+#           No.5              #
+###############################
+# Q: 题目：输入三个整数x,y,z，请把这三个数由小到大输出。
+# 程序分析：我们想办法把最小的数放到x上，先将x与y进行比较，如果x>y则将x与y的值进行交换，
+# 然后再用x与z进行比较，如果x>z则将x与z的值进行交换，这样能使x最小。
+def reverse():
+    res = []
+    while True:
+        for i in range(3):
+            num = int(raw_input('please insert a num: '))
+            res.append(num)
+    
+        return sorted(res)
+
+print(reverse())
+
+###############################
+#           No.6              #
+###############################
+'''题目：斐波那契数列。
+
+程序分析：斐波那契数列（Fibonacci sequence），又称黄金分割数列，指的是这样一个数列：0、1、1、2、3、5、8、13、21、34、……。
+
+在数学上，费波那契数列是以递归的方法来定义：'''
+
+def print_fib(n):
+    # 0、1、1、2、3、5、8、13、21、34、……    
+    fiblist = [1, 1]
+    if n <= 1:
+        return [1]
+    elif n == 2:
+        return fiblist
+    
+    for i in range(2,n):
+        fiblist.append(fiblist[-1] + fiblist[-2])
+    return fiblist
+    
+print print_fib(9)
+
+
+def get_fib(n):
+    a, b = 1, 1
+    for i in range(2,n):
+        a, b = a+b, a
+    return a
+
+def re_fib(n):
+    if n < 3:
+        return 1
+    else:
+        return re_fib(n-1) + re_fib(n-2)
+
+print re_fib(9)
+
+
+###############################
+#           No.7              #
+###############################
+'''
+题目：将一个列表的数据复制到另一个列表中。
+
+程序分析：使用列表[:]。'''
+def copy(a):
+    return a[:]
+
+print(copy([1,2,3,4,5,6]))
+
+
+###############################
+#           No.8              #
+###############################
+'''
+题目：输出 9*9 乘法口诀表。
+
+程序分析：分行与列考虑，共9行9列，i控制行，j控制列。'''
+for i in range(1,10):
+    print
+    for j in range(1,i+1):
+        print('%d*%d=%d' % (i, j, i*j))
+        
+
+###############################
+#           No.9              #
+###############################
+    
+'''题目：暂停一秒输出。
+
+程序分析：使用 time 模块的 sleep() 函数。'''
+import time
+def print_n():
+    for i in range(3):
+        print(i)
+        print
+        time.sleep(1)
+
+print_n()
+
+###############################
+#           No.11             #
+###############################
+    
+'''题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，
+小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+
+程序分析：兔子的规律为数列1,1,2,3,5,8,13,21.... '''
+# decompose: f1, f2, f3 and how their value change by each month
+def get_rab(n_month):
+    if n_month < 3:
+        return 1
+    f1, f2 = 1, 1
+    for i in range(n_month-1):
+        f1, f2 = f2, f1+f2
+    return f1
+      
+get_rab(7)
+
+
+###############################
+#           No.12             #
+###############################
+    
+'''题目：判断101-200之间有多少个素数，并输出所有素数。
+
+程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，
+如果能被整除，则表明此数不是素数，反之是素数。 '''
+
+def prime():
+    total = 0
+    for num in range(101, 201):
+        for divided in range(2, num):
+            if num % divided == 0:
+                # not prime num
+                break
+        # mistake made here
+        
+        else:
+            print num
+            total += 1
+    return total
+        
+print(prime())
+ 
+###############################
+#           No.13             #
+###############################
+    
+'''题目：打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，
+其各位数字立方和等于该数本身。
+例如：153是一个"水仙花数"，因为153=1的三次方＋5的三次方＋3的三次方。
+
+程序分析：利用for循环控制100-999个数，每个数分解出个位，十位，百位。''' 
+
+def find():
+    for num in range(100,1000):
+        hun = num / 100
+        tens = num % 100 / 10
+        units = num % 10
+        if num == hun*hun*hun + tens*tens*tens + units*units*units:
+            print num
+            
+find()
+
+###############################
+#           No.14             #
+###############################
+    
+'''题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
+
+程序分析：对n进行分解质因数，应先找到一个最小的质数k，然后按下述步骤完成：
+(1)如果这个质数恰等于n，则说明分解质因数的过程已经结束，打印出即可。
+(2)如果n<>k，但n能被k整除，则应打印出k的值，并用n除以k的商,作为新的正整数你n,
+重复执行第一步。
+(3)如果n不能被k整除，则用k+1作为k的值,重复执行第一步。'''
+def factor(num):
+    res = []
+    while num > 1:
+        for i in range(2,num+1):
+            if num%i == 0:
+                # a factor
+                num /= i
+                res.append(i)
+                break
+    return res
+print(factor(190))
+
+
+###############################
+#           No.15             #
+###############################
+'''
+题目：利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，
+        60-89分之间的用B表示，
+        60分以下的用C表示。
+
+程序分析：程序分析：(a>b)?a:b这是条件运算符的基本例子。'''
+def cut(grade):
+    if grade >= 90:
+        return 'A'
+    elif grade >= 60:
+        return 'B'
+    else:
+        return 'C'
+cut(89)
+
+###############################
+#           No.16             #
+###############################
+    
+'''题目：输出指定格式的日期。
+
+程序分析：使用 datetime 模块。'''
+import time, datetime
+print datetime.date.today().strftime('%d-%m-%Y')
+bd = datetime.date(1999,9,8)
+next_d = bd + datetime.timedelta(1)
+print next_d
+
+###############################
+#           No.17             #
+###############################
+    
+'''题目：输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
+
+程序分析：利用 while 或 for 语句,条件为输入的字符不为 '\n'。'''
+def analysis(string):
+    alpha, space, digit, other = 0, 0, 0, 0
+    for rune in string:
+        if rune.isalpha():
+            alpha += 1
+        elif rune.isdigit():
+            digit += 1
+        elif rune.isspace():
+            space += 1
+        else:
+            other += 1
+    print('%d letters, %d digits, %d spaces, %d others' % (
+            alpha, digit, space, other))
+
+analysis('Today is 10 10 2018.')
+
+###############################
+#           No.18             #
+###############################
+    
+'''题目：求s=a+aa+aaa+aaaa+aa...a的值，其中a是一个数字。
+例如2+22+222+2222+22222(此时共有5个数相加)，几个数相加由键盘控制。
+
+程序分析：关键是计算出每一项的值。
+'''
+# key format the string
+
+
+
 
 
 
